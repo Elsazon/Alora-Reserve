5d6d79e1d1f40e80eab5af242f7efd23f5bb5614
Elsazon <91290436+Elsazon@users.noreply.github.com>
2026-03-23 08:24:59 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwuMxvZooqa-2karAoFrDUKN_oTbzhLLWAREYe0xy2BTZ83m6bnKEMI9yc5Wn3m_6XP/exec",
+  SECRET: "demo123"
+};
