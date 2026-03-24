fc3628a3eea1ed72afb335ab3a870addbf0985fe
Elsazon <91290436+Elsazon@users.noreply.github.com>
2026-03-24 09:39:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbyu0KJhTxvp21imPFE6DwU0k-Fp9NuvKBtukbmuPFlHNqXRqTZF-CZ8qqjMrcG1ZHCA/exec",
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwjHnYR4l7RrpweQPnp7VQWQ-4kqiD0DuFSa3BHd02q447vqLbLefwrEF1NXVT279Fm/exec",
   SECRET: "demo123"
 };
