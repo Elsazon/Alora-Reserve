cfd2aa97f0eaa44aa927434bc04268483424879c
Elsazon <91290436+Elsazon@users.noreply.github.com>
2026-03-26 11:01:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbz8sQe5e2HALxsM1cymYnfL5JdzP6J7K477tKEQNuk4pIHbxR_sMw1Bj0jkwY7xPxI4/exec",
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycby8KZEEY0umi3UsjvI32f6xdZKWp--oS-IVVxkP4Ulj5ktgDsBlXJ_xgd4UWR7aN7wf/exec",
   SECRET: "demo123"
 };
