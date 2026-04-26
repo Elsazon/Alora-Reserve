cb2372cdc01658250253c5c9d8324d612f6987a9
Elsazon <91290436+Elsazon@users.noreply.github.com>
2026-04-26 07:31:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzBO4VyaLExFY7ZG69apoyGrkM3oEkkGOF8uMTrUA8Z4jQtthl0abwTmWpv2MWyFClw/exec",
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzx5ndxhrqryXtVhfaLFdqcTSAC6nTtZRPi4BhLVZX4cWd5Pq_CpTiBM8IqjluvLjBO/exec",
   SECRET: "demo123",
   PIN: "1234"
 };
