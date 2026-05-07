0e5422ec13ddc11cf79478780444bbc8ba9d517e
Elsazon <91290436+Elsazon@users.noreply.github.com>
2026-05-07 08:17:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwMhf1UP7Z2-NgRVCtg7QwxZ3J2MLcJCBI2_aRDz_7VaCq2Dk78Jc_H4YhulI8DENjR/exec",
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzoViaVlaufDn551Q7JrZnQxFNK6fE60srk2YccLfzBtQ6hiM494UJKOdoO2_GTX1Xu/exec",
   SECRET: "demo123",
   PIN: "1234"
 };
